f0fbf86552e0d656d24dd0a5c31f76545cbaf44a
LeeTaeHoon97 <dlxogns1004@gmail.com>
2022-05-06 20:44:09 +0900

    Update loss.py
    iou 구현

--- loss.py
+++ loss.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-
+import torch
 #본 모델은 13x13의 그리드셀과 5개의 앵커박스, 26개의 클래스로 이루어짐
 #S=grid cell , B= anchor box or bounding box, C= num of class
 class YoloLoss(nn.Module):
@@ -14,8 +14,32 @@ class YoloLoss(nn.Module):
         self.lambda_zcoord=0.3        #단안카메라 거리측정에 쓰일 loss balance parm
     
     def forward(self,pred,target):
+        pass
+
+
+
+    def iou(self,pred,target):          #kitti dataset은 midpoint 형식의 바운딩박스를 가짐.
+                                        #shape = [x,y,w,h]
+        pred_box_x1=pred[0:1]-pred[2:3]/2
+        pred_box_y1=pred[1:2]-pred[3:4]/2
+        pred_box_x2=pred[0:1]+pred[2:3]/2
+        pred_box_y2=pred[1:2]+pred[3:4]/2
+
+        target_box_x1=target[0:1]-target[2:3]/2
+        target_box_y1=target[1:2]-target[3:4]/2
+        target_box_x2=target[0:1]+target[2:3]/2
+        target_box_y2=target[1:2]+target[3:4]/2
         
+        #두 사각형의 겹치는 면적 좌표 구하기
+        x1=torch.max(pred_box_x1,target_box_x1)
+        y1=torch.max(pred_box_y1,target_box_y1)
+        x2=torch.max(pred_box_x2,target_box_x2)
+        y2=torch.max(pred_box_y2,target_box_y2)
 
+        intersection=(x2-x1).clamp(0) * (y2-y1).clamp(0)        #torch.clamp(0)을 사용, 만약 두 사각형이 겹치지 않는경우 neg~0사이의 값이 나오므로 이를 0으로 조정 해줌.
 
+        pred_box_area=abs((pred_box_x2-pred_box_x1)*(pred_box_y2-pred_box_y1))
+        target_box_area=abs((target_box_x2-target_box_x1)*(target_box_y2-target_box_y1))
 
+        return intersection/(pred_box_area+target_box_area-intersection+ 1e-6)
 
